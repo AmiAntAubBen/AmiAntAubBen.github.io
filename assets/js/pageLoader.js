25c883607d394ed1314d10b0cd2d9febf413c36f
AminaDridi <amn.dridi@gmail.com>
2022-01-23 20:42:12 +0100

    visu

--- assets/js/pageLoader.js
+++ assets/js/pageLoader.js
@@ -60,19 +60,7 @@ async function loadVisualisationComponent(idVisu, componentURL){
 }
 
 function loadVisualisationScript(idVisu, jsContent) {
-    var setInnerHTML = function(elm, html) {
-        elm.innerHTML = html;
-        Array.from(elm.querySelectorAll("script")).forEach( oldScript => {
-            const newScript = document.createElement("script");
-            Array.from(oldScript.attributes)
-                .forEach( attr => newScript.setAttribute(attr.name, attr.value) );
-            newScript.appendChild(document.createTextNode(oldScript.innerHTML));
-            oldScript.parentNode.replaceChild(newScript, oldScript);
-        });
-    }
-    let elm = document.querySelector("#visu" + idVisu);
-    //document.querySelector("#visu" + idVisu).innerHTML += "<script id=\"scriptVisu " + idVisu + "\">" + jsContent + "</script>";
-    setInnerHTML(elm,jsContent)
+    document.querySelector("#visu" + idVisu).innerHTML += "<script id=\"scriptVisu " + idVisu + "\">" + jsContent + "</script>";
 
     //document.querySelector("#scriptVisu" + idVisu)).append();
 }
